83adad3d730679542ed7555f151db160a255919e
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-11-25 03:44:45 +0300

    create: public file

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+import history from 'connect-history-api-fallback';
+import path from 'path';
+
 const app = express();
 
+//middlewares
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(fileUpload({useTempFiles: true}));
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname);
+
+//routes
 app.get('/', (req, res) =>{
     res.send('Hello World');
 })
